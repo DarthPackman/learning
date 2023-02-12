dc2acb833ac894ca0dddb1fe0d99f3cd42328561
Gavin Edwards <90949133+DarthPackman@users.noreply.github.com>
2023-02-11 19:09:58 -0800

    Update run.py

--- Calculator/run.py
+++ Calculator/run.py
@@ -1,13 +1,20 @@
+#Function to calculate the sum of two numbers
 def add(a, b):
     return a + b
 
+#Function to calculate the difference of two numbers
 def multiply(a,b):
     return a * b
 
+#Function to divide one number by another
 def divison(a,b):
     return a/b
+
+#Function to multiply one number by another
 def subtraction(a,b):
     return a-b
+
+#Main Function
 def main():
     print("Hello World! BLARG")
     print("Hell World")
